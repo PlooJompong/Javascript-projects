38565a25403f86f495ad9096008382286cd8fc38
Ploo Jompong <ploo.jompong@hotmail.com>
2024-01-15 16:14:16 +0100

    Dice Roller Done

--- Dice-Roller/index.js
+++ Dice-Roller/index.js
@@ -1,39 +1,39 @@
-const ROLL_DICE_BTN = document.querySelector("#roll-dice-btn");
+const DISPLAY_RESULT = document.querySelector("#result");
 const DICE_ONE = document.querySelector("#dice-one");
 const DICE_TWO = document.querySelector("#dice-two");
+const ROLL_DICE_BTN = document.querySelector("#roll-dice-btn");
 
-let diceOne;
-let diceTwo;
+if (!DISPLAY_RESULT || !DICE_ONE || !DICE_TWO || !ROLL_DICE_BTN) {
+  console.error("One or more elements not found.");
+} else {
+  let diceOne;
+  let diceTwo;
 
-function rollDice() {
-  diceOne = Math.floor(Math.random() * 6 + 1);
-  diceTwo = Math.floor(Math.random() * 6 + 1);
-  displayDice();
-  checkResult();
-}
+  function rollDice() {
+    randomNumber();
+    displayDice();
+    checkResult();
+  }
 
-function displayDice() {
-  const diceElements = [DICE_ONE, DICE_TWO];
-  const diceValues = [diceOne, diceTwo];
+  function randomNumber() {
+    diceOne = Math.floor(Math.random() * 6 + 1);
+    diceTwo = Math.floor(Math.random() * 6 + 1);
+  }
 
-  for (let i = 0; i < diceElements.length; i++) {
-    const diceElement = diceElements[i];
-    const diceValue = diceValues[i];
+  function displayDice() {
+    DICE_ONE.setAttribute("src", `assets/dice${diceOne}.png`);
+    DICE_TWO.setAttribute("src", `assets/dice${diceTwo}.png`);
+  }
 
-    if (diceValue >= 1 && diceValue <= 6) {
-      diceElement.setAttribute("src", `assets/dice${diceValue}.png`);
+  function checkResult() {
+    if (diceOne > diceTwo) {
+      DISPLAY_RESULT.innerText = "Player 1 wins!";
+    } else if (diceOne < diceTwo) {
+      DISPLAY_RESULT.innerText = "Player 2 wins!";
+    } else {
+      DISPLAY_RESULT.innerText = "Draw!";
     }
   }
-}
 
-function checkResult() {
-  if (diceOne < diceTwo) {
-    console.log("Player 2 wins");
-  } else if (diceOne > diceTwo) {
-    console.log("Player 1 wins");
-  } else {
-    console.log("It's a tie");
-  }
+  ROLL_DICE_BTN.addEventListener("click", rollDice);
 }
-
-ROLL_DICE_BTN.addEventListener("click", rollDice);
