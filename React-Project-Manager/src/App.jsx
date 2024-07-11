8043edc296753293cd7fac1237c53e4454c9af84
Ploo Jompong <ploo.jompong@hotmail.com>
2024-07-11 10:46:17 +0200

    medal, validation, cancelBtn

--- React-Project-Manager/src/App.jsx
+++ React-Project-Manager/src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     });
   }
 
+  function handleCancelProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectsState((prevState) => {
       const projectId = Math.random();
@@ -38,7 +47,9 @@ function App() {
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelProject} />
+    );
   } else if (projectsState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
--- React-Project-Manager/src/components/NewProject.jsx
+++ React-Project-Manager/src/components/NewProject.jsx
@@ -1,7 +1,10 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-function NewProject({ onAdd }) {
+function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
+
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
@@ -11,6 +14,15 @@ function NewProject({ onAdd }) {
     const enteredDescription = descriptionRef.current.value;
     const enteredDueDate = dueDateRef.current.value;
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
+
     onAdd({
       title: enteredTitle,
       description: enteredDescription,
@@ -19,28 +31,43 @@ function NewProject({ onAdd }) {
   }
 
   return (
-    <div className="mt-16 w-[35rem]">
-      <menu className="my-4 flex items-center justify-end gap-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950">
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            className="rounded-md bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950"
-            onClick={handleSave}
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input type="text" ref={titleRef} label="Title" />
-        <Input ref={descriptionRef} label="Description" istextArea />
-        <Input type="date" ref={dueDateRef} label="Due Date" />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2 className="my-4 text-xl font-bold text-stone-700">Invalid Input</h2>
+        <p className="mb-4 text-stone-600">
+          Oops... looks like you forgot to enter a value.
+        </p>
+        <p className="mb-4 text-stone-600">
+          Please make sure you provide a valid value for every input field.
+        </p>
+      </Modal>
+
+      <div className="mt-16 w-[35rem]">
+        <menu className="my-4 flex items-center justify-end gap-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              className="rounded-md bg-stone-800 px-6 py-2 text-stone-50 hover:bg-stone-950"
+              onClick={handleSave}
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input type="text" ref={titleRef} label="Title" />
+          <Input ref={descriptionRef} label="Description" isTextArea />
+          <Input type="date" ref={dueDateRef} label="Due Date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
